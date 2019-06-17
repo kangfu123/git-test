ef4137b89415e2339b116696ead4564d6e945a50
kangfu <1243780083@qq.com>
2019-06-17 10:57:01 +0800

    添加输出你的眼神

--- git/src/cn/itcast/FirstClass.java
+++ git/src/cn/itcast/FirstClass.java
@@ -10,6 +10,7 @@ public class FirstClass {
         System.out.println("睁开双眼做场梦");
         System.out.println("睁开双眼做场梦");
         System.out.println("心安理得");
+        System.out.println("你的眼神");
 
     }
 }
