3d34644658bc8d3c06bebb4a310df617abff672a
kangfu <1243780083@qq.com>
2019-06-17 10:40:12 +0800

    提交git项目

---
+++ git/src/cn/itcast/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+        System.out.println("睁开双眼做场梦");
+
+    }
+}
